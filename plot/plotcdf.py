a933faee26c3e884971969804063f4fd469853ff
Antonio Lima <anto87@gmail.com>
2014-05-11 14:47:29 +0100

    Unified function for pdf/cdf

--- plot/plotcdf.py
+++ plot/plotcdf.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
-"""Plot PDF.
+"""Plot CDF.
 
 Usage:
-  plotpdf FILE [options]
+  plotcdf FILE [options]
 
 Options:
   -t STR --title=STR      title
   -x STR --xlabel=STR     x axis label [default: Values]
   -y STR --ylabel=STR     y axis label [default: CDF]
   -b INT --nbins=INT      number of bins
+  -lb --logbin            logarithmic binning
 
 """
 
@@ -25,15 +26,15 @@ import settings
 from utils import common_settings
 #settings.apply_settings(mpl)
 
+import plotpdf
+
 if __name__ == "__main__":
     args = docopt(__doc__, version='Plot 0.1')
     finput = fileinput.FileInput(openhook=fileinput.hook_compressed)
     data = [float(i) for i in finput]
-    if not args['--nbins']:
-        args['--nbins'] = 1+ np.log2(len(data))
-    n, bins = np.histogram(data, bins=args['--nbins'], normed=True)
-    n = np.cumsum(n*np.diff(bins))
+    n, bin_edges = plotpdf.histogram(args, data)
+    n = np.cumsum(n*np.diff(bin_edges))
 
     common_settings(args, plt)
-    plt.plot(bins[1:], n, 'o')
+    plt.plot(bin_edges[1:], n, 'o')
     plt.show()
--- plot/plotpdf.py
+++ plot/plotpdf.py
@@ -10,7 +10,8 @@ Options:
   -t STR --title=STR      title
   -x STR --xlabel=STR     x axis label [default: Values]
   -y STR --ylabel=STR     y axis label [default: CDF]
-  -b INT --nbins=INT      Number of bins
+  -b INT --nbins=INT      number of bins
+  -lb --logbin            logarithmic binning
 
 """
 
@@ -25,14 +26,22 @@ import settings
 #settings.apply_settings(mpl)
 from utils import common_settings
 
+def histogram(args, data):
+    left, right = min(data), max(data)
+    if not args['--nbins']:
+        args['--nbins'] = 1+ np.log2(len(data))
+    if args['--logbin']:
+        bins = np.logspace(left, right, args['--nbins'])
+    else:
+        bins = np.linspace(left, right, args['--nbins'])
+    return np.histogram(data, bins=bins, density=True)
+
 if __name__ == "__main__":
     args = docopt(__doc__, version='Plot 0.1')
     finput = fileinput.FileInput(openhook=fileinput.hook_compressed)
     data = [float(i) for i in finput]
-    if not args['--nbins']:
-        args['--nbins'] = 1+ np.log2(len(data))
-    n, bins = np.histogram(data, bins=args['--nbins'], normed=True)
+    n, bin_edges = histogram(args, data)
 
     common_settings(args, plt)
-    plt.plot(bins[1:], n, 'o')
+    plt.plot(bin_edges[1:], n, 'o')
     plt.show()
